7602c8d6c180fb4a5f8230419a612d6c38362b74
woonet <woo9347@gmail.com>
2020-09-02 09:38:31 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Sampledata from "./fire/SampleData";
+import firebase from "firebase";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Firebaseの設定
+const firebaseConfig = {
+  apiKey: "XXXxxx999",
+  authDomain: "xxx.firebaseapp.com",
+  databaseURL: "https://xxx.firebaseio.com",
+  projectId: "xxx",
+  storageBucket: "xxx.appspot.com",
+  messagingSenderId: "999",
+  appId: "1:999:web:000xxx",
+  measurementId: "G-XXX",
+};
+
+// Firebaseの初期化
+firebase.initializeApp(firebaseConfig);
+
+// Appコンポーネント
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Fire</h1>
+        <h2>Sample data.</h2>
+        <Sampledata />
+      </div>
+    );
+  }
 }
 
 export default App;
